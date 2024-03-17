3ceffe8b753e224d0a7e9e23b6d11da518204c45
LEUNSU <158007401+LEUNSU@users.noreply.github.com>
2024-03-17 17:56:54 +0900

    Create input01.c

---
+++ day01/input01.c
@@ -0,0 +1,29 @@
+ #include <stdio.h>
+ 
+  void main()
+  {
+    int n;
+    printf("정수 하나입력하세요: ");
+    scanf("%d", &n);
+ 
+    if(n == 1)
+    {
+    printf("1을 입력하셨습니다.\n");
+    }
+    else if(n == 2)
+    {
+    printf("2를 입력하셨습니다.\n");
+    }
+    else if(n == 3)
+    {
+    printf("3을 입력하셨습니다.\n");
+    }
+    else if(n == 4)
+    {
+    printf("4를 입력하셨습니다.\n");
+    }
+    else if(n == 5)
+    {
+    printf("5를 입력하셨습니다.\n");
+    }
+  }
