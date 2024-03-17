e457ede1feb02ef8ce56eed20149dac2cf29ee27
LEUNSU <158007401+LEUNSU@users.noreply.github.com>
2024-03-17 17:52:25 +0900

    Create func02.c

---
+++ day01/func02.c
@@ -0,0 +1,18 @@
+  #include <stdio.h>
+ 
+  void main()
+  {
+    int 10, 20;
+    sumf(10, 20);
+    minusf(20, 10);
+ 
+  //  printf(     // 출력을 메인에서 한다.
+  }
+
+  //"더한 값은 30이고 뺀 값은 10입니다."
+
+  int add(a, b)
+  {
+    a+b;
+    return 0;
+  }
