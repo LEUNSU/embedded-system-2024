4c734b56755f259ee4b32f7eca9245374e1915cb
LEUNSU <158007401+LEUNSU@users.noreply.github.com>
2024-03-19 12:04:24 +0900

    Create queue_test01.c

---
+++ day04/queue_test01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+  int ary[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int queue[20];
+  int rear= 0, front = 0; // 입구(rear)로 삽입하고, 출구(front)를 통해서>
+
+  int size = size(ary) / sizeof(ary[0]);
+  // 큐에 삽입
+  for(int i = 0; i < size; i++){
+    queue[rear++] = ary[i];
+  }
+
+  // 큐에서 삭제
+  for(int i = 0; i < size; i++){
+    printf("%d\n", queue[front++]);
+  }
+}
